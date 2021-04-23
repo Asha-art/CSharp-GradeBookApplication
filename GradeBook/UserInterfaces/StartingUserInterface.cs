8bd5a9c448ddf7ff9f19c7630255546b86b56487
Asha-art <62064699+Asha-art@users.noreply.github.com>
2021-04-23 19:04:25 -0400

    update StartingUserInterface with add return statement

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -37,7 +37,7 @@ namespace GradeBook.UserInterfaces
             if (parts.Length != 3)
             {
                 Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
-              
+                return;              
             }
             var name = parts[1];
 
