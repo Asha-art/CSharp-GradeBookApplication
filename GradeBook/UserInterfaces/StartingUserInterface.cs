798be31d929d7fee3a55299d763960bbb6f70351
Asha-art <62064699+Asha-art@users.noreply.github.com>
2021-04-23 19:01:56 -0400

    update StartingUserInterface with initiated gradeBook

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -41,23 +41,23 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];
 
-            var type = parts[2].ToLower();
-            BaseGradeBook gradeBook;
+            var type = parts[2];
+            BaseGradeBook gradeBook = new BaseGradeBook(name);
 
             if (type == "standard")
-            {
+
                 gradeBook = new StandardGradeBook(name);
-            }
+
             else if (type == "ranked")
-            {
+
                 gradeBook = new RankedGradeBook(name);
-            }
+
             else
             {
-                Console.WriteLine( type + " is not a supported type of gradebook, please try again.");
+                Console.WriteLine(type + " is not a supported type of gradebook, please try again.");
+                return;
             }
 
-            gradeBook = new BaseGradeBook(name);
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
